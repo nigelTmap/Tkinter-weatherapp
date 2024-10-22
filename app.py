cdcfd06de294d83e7719bd4051c468500908288e
NigelTmap <tnigelmap09@gmail.com>
2024-10-22 04:31:01 +0200

    fetch data achieved

--- app.py
+++ app.py
@@ -18,23 +18,24 @@ def get_weather(city):
         data = result.json()
         
         name = data['name']
-        description = data['weather'][0]['description']
-       # icon = data['weather'][0]['icon']
         temp = data['main']['temp']
-        final = (name,description,temp )
+        icon = data['weather'][0]['icon']
+        description = data['weather'][0]['description']
+        
+        final = (name,temp,icon,description)
         return final
     else:
         None
-
+print(get_weather('london'))
 
 def search():
     city = city_text.get()
     weather = get_weather(city)
     if weather:
         location_lbl['text']= '{}'.format(weather[0])
-     #   img_lbl['bitmap']='images/{}.png'.format(weather[2])
-        temp_lbl['text']= '{:.2f}C'.format(weather[3])
-        weather_lbl['text']= weather[1]
+        temp_lbl['text']= '{:.2f}C'.format(weather[1])
+        img_lbl['bitmap']='images/{}.png'.format(weather[2])
+        weather_lbl['text']= weather[3]
     
     else:
         messagebox.showerror('Error','cannot find city{}'.format(city))
@@ -53,11 +54,11 @@ search_btn.pack()
 location_lbl = Label(root,text='' ,font={'bold',20})
 location_lbl.pack()
 
-#img_lbl = Label(root,bitmap='')
-#img_lbl.pack()
-
 temp_lbl = Label(root,text='')
 temp_lbl.pack()
+
+img_lbl = Label(root,bitmap='')
+img_lbl.pack()
 
 weather_lbl = Label(root,text='')
 weather_lbl.pack()
