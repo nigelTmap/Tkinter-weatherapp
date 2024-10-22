26cf8e38616c77a20db3d4741487599481f8a1de
NigelTmap <tnigelmap09@gmail.com>
2024-10-22 06:16:23 +0200

    app complete

--- app.py
+++ app.py
@@ -18,49 +18,59 @@ def get_weather(city):
         data = result.json()
         
         name = data['name']
+        country = data['sys']['country']
         temp = data['main']['temp']
+        temp_cel = temp - 273.15
+        temp_f = (temp - 273.15)* 9/5 + 32
         icon = data['weather'][0]['icon']
         description = data['weather'][0]['description']
+        humidity = data['main']['humidity']
         
-        final = (name,temp,icon,description)
+        final = (name,temp_cel,icon,description,temp_f,humidity,country)
         return final
     else:
         None
-print(get_weather('london'))
 
 def search():
     city = city_text.get()
     weather = get_weather(city)
     if weather:
-        location_lbl['text']= '{}'.format(weather[0])
-        temp_lbl['text']= '{:.2f}C'.format(weather[1])
-        img_lbl['bitmap']='images/{}.png'.format(weather[2])
-        weather_lbl['text']= weather[3]
+        location_lbl['text']= '{}, {}'.format(weather[0],weather[6])
+        temp_lbl['text']= '{:.2f}C, {:.2f}F'.format(weather[1],weather[4])
+        icon_ref = PhotoImage(file='images/{}.png'.format(weather[2]))
+        image['image'] = icon_ref
+        image.image=icon_ref
+        weather_lbl['text']= '{}, Humidity:{}%'.format(weather[3],weather[5])
     
     else:
         messagebox.showerror('Error','cannot find city{}'.format(city))
 
 root = Tk()
 root.title("weather app")
-root.geometry('700x350')
+root.geometry('460x560')
 
 city_text = StringVar()
 city_entry = Entry(root, textvariable=city_text)
 city_entry.pack()
+Label(root).pack()
 
 search_btn = Button(root,text='search',width=12 ,command=search)
 search_btn.pack()
+Label(root).pack()
+Label(root).pack()
 
-location_lbl = Label(root,text='' ,font={'bold',20})
+location_lbl = Label(root,text='' ,font=('arial',26))
 location_lbl.pack()
 
-temp_lbl = Label(root,text='')
+Label(root).pack()
+
+temp_lbl = Label(root,text='',font =('arial',48))
 temp_lbl.pack()
 
-img_lbl = Label(root,bitmap='')
-img_lbl.pack()
+image = Label(root,image='')
+image.pack()
 
-weather_lbl = Label(root,text='')
+weather_lbl = Label(root,text='',font=('arial',20))
 weather_lbl.pack()
 
 
